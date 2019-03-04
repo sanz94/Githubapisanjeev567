32cc57cd4036686a87390f09c2d87ce2b9ff8d3e
Sanjeev Rajasekaran <sanjeev95@rocketmail.com>
2019-03-04 13:27:47 -0500

    Update hw04sanjeev.py

--- hw04sanjeev.py
+++ hw04sanjeev.py
@@ -1,15 +1,16 @@
-import requests
 import unittest
 import json
 import urllib
+import requests
 
-def parseGit(user_id, optional_param= "repoCommits"):
+
+def parse_git(user_id, optional_param= "repoCommits"):
     repo_list = []
     try:
-        with urllib.request.urlopen("https://api.github.com/users/"+user_id+"/repos") as reporesponse:
+        with urllib.request.urlopen("https://aasdasdadasdpi.github.com/users/"+user_id+"/repos") as reporesponse:
             repohtml = reporesponse.read()
             repoans = json.loads(repohtml)
-    except:
+    except urllib.error.HTTPError:
         return "Invalid URL".format("https://api.github.com/users/"+user_id+"/repos")
 
     print("User {} has {} repositories".format(user_id, str(len(repoans))))
@@ -21,8 +22,8 @@ def parseGit(user_id, optional_param= "repoCommits"):
             with urllib.request.urlopen("https://api.github.com/repos/" + user_id + "/" + repo["name"] + "/commits") as commitresponse:
                 commithtml = commitresponse.read()
                 commitans = json.loads(commithtml)
-        except:
-            return "Invalid URL {}".format("https://api.github.com/repos/" + user_id + "/" + repo["name"] + "/commits")
+        except urllib.error.HTTPError:
+            return "Invalid URL".format("https://api.github.com/repos/" + user_id + "/" + repo["name"] + "/commits")
 
         print("Repo: {} Number of commits: {}".format(repo["name"], len(commitans)))
     if optional_param == "repocount":
@@ -31,17 +32,27 @@ def parseGit(user_id, optional_param= "repoCommits"):
 
         return repo_list
 
+
 class TestCases(unittest.TestCase):
+
+    def testInvalid(self):
+        self.assertEqual(parse_git("invalidinvalidinvalid", "repoCount"), 'Invalid URL')
+        self.assertEqual(parse_git("invalidinvalidinvalid", "repoCommits"), 'Invalid URL')
 
     def testRepoCount(self):
 
-        self.assertEqual(parseGit("sanz94", "repocount"), 15)
+        self.assertEqual(parse_git("sanz94", "repocount"), 15)
 
     def testRepoList(self):
 
-        self.assertEqual(parseGit("sanz94", "repoCommits"), ['CPE593_2017F', 'Daizo-AI-chatbot', 'Githubapisanjeev567', 'Kriya-website', 'QtClientServerCPP', 'RLAI', 'Rain-Data', 'Smart-Home', 'SSW-555', 'SSW567', 'Student-Database', 'TrackPro-Tracking-app', 'Triangle567', 'University-Repository', 'Wall-E'])
+        self.assertEqual(parse_git("sanz94", "repoCommits"), ['CPE593_2017F', 'Daizo-AI-chatbot',
+                                                              'Githubapisanjeev567', 'Kriya-website',
+                                                              'QtClientServerCPP', 'Rain-Data', 'RLAI',
+                                                              'Smart-Home', 'SSW-555', 'SSW567', 'Student-Database',
+                                                              'TrackPro-Tracking-app', 'Triangle567',
+                                                              'University-Repository', 'Wall-E'])
 
 
 if __name__ == '__main__':
     unittest.main()
-print(parseGit("sanz94"))
+print(parse_git("sanz94"))
